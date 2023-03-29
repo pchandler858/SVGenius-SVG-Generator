5f04ad1037b67662e2fc7c4861a810ba328e6efc
Preston C <pchandler858@gmail.com>
2023-03-29 15:56:47 -0700

    completed prompts

--- index.js
+++ index.js
@@ -2,3 +2,34 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const { Circle, Triangle, Square } = require("./lib/shapes.js");
+
+function userPrompt() {
+  return new Promise((resolve, reject) => {
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "text",
+        message: "Enter up to 3 characters for text:",
+        validate: function (input) {
+          return input.length <= 3 || "Please enter 3 characters max";
+        },
+      },
+      {
+        type: "input",
+        name: "textColor",
+        message: "Enter a color or hex # for your text:",
+      },
+      {
+        type: "list",
+        name: "shape",
+        message: "Pick a shape:",
+        choices: ["circle", "triangle", "square"],
+      },
+      {
+        type: "input",
+        name: "shapeColor",
+        message: "Enter a color or hex # for your shape:",
+      },
+    ]);
+  });
+}
