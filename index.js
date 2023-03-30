62eb02da89e917c84ef045f9176367c4b9f3f435
Preston C <pchandler858@gmail.com>
2023-03-29 19:51:14 -0700

    mvp ready

--- index.js
+++ index.js
@@ -1,65 +1,72 @@
 // Import modules
 const inquirer = require("inquirer");
 const fs = require("fs");
-const { Shape, Circle, Triangle, Square } = require("./lib/shapes.js");
-const { resolve } = require("path");
+const { Circle, Triangle, Square } = require("./lib/shapes.js");
 
 function userPrompt() {
   return new Promise((resolve, reject) => {
-    inquirer.prompt([
-      {
-        type: "input",
-        name: "text",
-        message: "Enter up to 3 characters:",
-        // validate that user has entered at least 3 characters. If not throw validation message until user corrects.
-        validate: function (input) {
-          return input.length <= 3 || "Please enter 3 characters max";
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "text",
+          message: "Enter up to 3 characters:",
+          // validate that user has entered at least 3 characters. If not throw validation message until user corrects.
+          validate: function (input) {
+            return input.length <= 3 || "Please enter 3 characters max";
+          },
         },
-      },
-      {
-        type: "input",
-        name: "textColor",
-        message: "Enter a color or hex # for your text:",
-      },
-      {
-        type: "list",
-        name: "shape",
-        message: "Pick a shape:",
-        choices: ["Circle", "Triangle", "Square"],
-      },
-      {
-        type: "input",
-        name: "shapeColor",
-        message: "Enter a color or hex # for your shape:",
-      },
-    ]);
-  })
-    .then((answers) => {
-      let shape;
-      switch (answers.shape) {
-        case "Circle":
-          shape = new Circle();
-          break;
-        case "Triangle":
-          shape = new Triangle();
-          break;
-        case "Square":
-          shape = new Square();
-          break;
-        default:
-          break;
-      }
-      shape.setColor(answers.shapeColor);
-      shape.setTextColor(answers.textColor);
+        {
+          type: "input",
+          name: "textColor",
+          message: "Enter a color or hex # for your text:",
+        },
+        {
+          type: "list",
+          name: "shape",
+          message: "Pick a shape:",
+          choices: ["Circle", "Triangle", "Square"],
+        },
+        {
+          type: "input",
+          name: "shapeColor",
+          message: "Enter a color or hex # for your shape:",
+        },
+      ])
+      .then((answers) => {
+        let shape;
+        switch (answers.shape) {
+          case "Circle":
+            shape = new Circle(answers.text);
+            break;
+          case "Triangle":
+            shape = new Triangle(answers.text);
+            break;
+          case "Square":
+            shape = new Square(answers.text);
+            break;
+          default:
+            break;
+        }
 
-      const svg = shape.render(answers.text);
+        shape.setColor(answers.shapeColor);
+        shape.setTextColor(answers.textColor);
 
-      fs.writeFile("logo.svg", svg, (err) => {
-        if (err) reject(err);
-        resolve("SVG Generated!!!");
+        const svg = shape.render(answers.text);
+
+        fs.writeFile("logo.svg", svg, (err) => {
+          if (err) {
+            console.error(err);
+            reject(err);
+          } else {
+            resolve("SVG Generated!!!");
+          }
+        });
+      })
+      .catch((error) => {
+        reject(error);
       });
-    })
-    .catch((error) => reject(error));
+  });
 }
 
 userPrompt()
